3b494567aad51b4339b3c029659904cc54150c21
Ed Mckain <mckain.default@gmail.com>
2017-12-13 17:29:11 +0100

    adds logic to the rest of the endpoints

--- main.go
+++ main.go
@@ -39,6 +39,37 @@ func main(){
 func GetPeople(w http.ResponseWriter, r *http.Request) {
   json.NewEncoder(w).Encode(people)
 }
-func GetPerson(w http.ResponseWriter, r *http.Request) {}
-func CreatePerson(w http.ResponseWriter, r *http.Request) {}
-func DeletePerson(w http.ResponseWriter, r *http.Request) {}
+
+func GetPerson(w http.ResponseWriter, r *http.Request) {
+  params := mux.Vars(r)
+  for _, item := range people {
+      if item.ID == params["id"] {
+          json.NewEncoder(w).Encode(item)
+          return
+      }
+  }
+
+  json.NewEncoder(w).Encode(&Person{})
+}
+
+func CreatePerson(w http.ResponseWriter, r *http.Request) {
+  params := mux.Vars(r)
+
+  var person Person
+  _ = json.NewDecoder(r.Body).Decode(&person)
+  person.ID = params["id"]
+
+  people = append(people, person)
+  json.NewEncoder(w).Encode(people)
+}
+
+func DeletePerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for index, item := range people {
+		if item.ID == params["id"] {
+			people = append(people[:index], people[index+1:]...)
+			break
+		}
+		json.NewEncoder(w).Encode(people)
+	}
+}
